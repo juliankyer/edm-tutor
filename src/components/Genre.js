f4cda2218459a1d088d66b3aad66577abeb08a1d
Julian Kyer <julian.kyer@gmail.com>
2017-04-14 09:41:13 -0600

    Refactor render in Genre

--- src/components/Genre.js
+++ src/components/Genre.js
@@ -1,11 +1,6 @@
 import React, { Component } from 'react';
 import Video from './Video';
-
-import houseVideos from '../data/houseVideos'; 
 import genres from '../data/genres';
-
-
-
 
 class Genre extends Component {
   constructor(props) {
@@ -22,7 +17,7 @@ class Genre extends Component {
     let videos = genres.filter((genre) => {
       return genre.link === target;
     });
-    this.setState({videos: videos})
+    this.setState({videos})
   }
   
   componentDidMount() {
@@ -33,24 +28,21 @@ class Genre extends Component {
     this.getVideos();
   }
   
-
+  goToVideo(index) {
+    let videoIndex = index;
+    if (videoIndex < 0) {
+      videoIndex = videoIndex.length - 1;
+    } else if (videoIndex >= this.state.videos.length) {
+      videoIndex = 0;
+    }
+    this.setState({
+      videoIndex
+    });
+  }
   
-  
-  // goToVideo(index) {
-  //   let videoIndex = index;
-  //   if (videoIndex < 0) {
-  //     videoIndex = videoIndex.length - 1;
-  //   } else if (videoIndex >= this.state.videos.length) {
-  //     videoIndex = 0;
-  //   }
-  //   this.setState({
-  //     videoIndex
-  //   });
-  // }
   render() {
-    // const { service, video, title, artist } = this.state.videos[this.state.videoIndex];
-    const { description, genre, id, link, videos } = this.state.videos[0];
-    const { } = this.state.videos[0].videos;
+    const { service, video, title, artist } = this.state.videos[0].videos[0];
+    const { description } = this.state.videos;
     return (
       <div className="genreWrapper">
         <h1>{this.state.videos[0].genre}</h1>
@@ -75,6 +67,43 @@ class Genre extends Component {
 
 
 export default Genre;
+
+// const { service, video, title, artist } = this.state.videos[this.state.videoIndex];
+// const { description, genre, id, link, videos } = this.state.videos[0];
+// const { } = this.state.videos[0].videos;
+
+
+// <div className="genreWrapper">
+//   <h1>{this.state.videos[0].genre}</h1>
+//   <Video service={service} video={video} width={700} height={350} />
+//   <div className="genre-description">
+//     <h4>"{title}" by {artist}</h4>
+//     <p>{description}</p>
+//   </div>
+//   <div className="button-wrapper">
+//     <button onClick={ this.goToVideo.bind(this, this.state.videoIndex - 1) }>
+//       Previous
+//     </button>
+//     <button onClick={ this.goToVideo.bind(this, this.state.videoIndex + 1) }>
+//       Next
+//     </button>
+//     <button className="love-button">Love</button>
+//   </div>
+// </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // <div className="genreWrapper">
 //   <h1>{genres[0].genre}</h1>
