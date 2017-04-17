adef9bdd29eb04524000c12309c71f829c6054c3
Julian Kyer <julian.kyer@gmail.com>
2017-04-17 15:13:08 -0600

    Refine CSS

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,22 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Navigation from './Navigation';
 
-const Contact = () => {
-  return (
-    <div className="contact-wrapper">
-      <Navigation />
-      <h1 className="contact-header">Contact</h1>
-      <p>Think we messed up somewhere?</p>
-      <p>Want to see something included?</p>
-      <p>Send us a note!</p>
-      <form className="contact-form">
-        <input type="text" placeholder="Name" />
-        <input type="email" placeholder="Email" />
-        <input className="email-body" type="text" placeholder="Message" />
-        <input className="submit-button" type="submit" />
-      </form>
-    </div>
-  )
+class Contact extends Component {
+  constructor() {
+    super();
+    this.state = {
+      text: ''
+    }
+  }
+  render() {
+    return (
+      <div className="contact-wrapper">
+        <Navigation />
+        <h1 className="contact-header">Contact</h1>
+        <p>Think we messed up somewhere?</p>
+        <p>Want to see something included?</p>
+        <a className="contact" href="mailto:julian.kyer@gmail.com?subject=EDM%20Tut.r"><p>Send us a note!</p></a>
+      </div>
+    )
+  }
 }
 
 export default Contact;
